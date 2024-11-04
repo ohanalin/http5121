c9aa33e00f64ef49e0c52c958a206e75e53732a5
Joyce Lin <ohanalin@gmail.com>
2024-11-04 16:57:52 -0500

    update

---
+++ week9/JS-LAB-7-FormValidation24F/Lab-7-forms.js
@@ -0,0 +1,12 @@
+/* LAB 7 - SHIPPING FORM */
+//Order Shipping object (for extra EXTRA challenge, otherwise, ignore it)
+var shipInfo = {
+	cid: 0,
+	name: "",
+	pc: "",
+	speed: "",
+	cost: 0
+};
+
+
+
